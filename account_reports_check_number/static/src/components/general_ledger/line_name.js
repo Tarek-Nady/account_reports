be45ee3bfc0c0c27f2d81dddd2ce56e0378e8484
Tarek_Nady <tarek.nady98@gmail.com>
2025-09-01 14:16:31 +0300

    ffff

--- account_reports_check_number/static/src/components/general_ledger/line_name.js
+++
@@ -1,7 +0,0 @@
-/** @odoo-module */
-
-import { AccountReportLineName } from "@account_reports/components/account_report/line_name/line_name";
-
-export class GeneralLedgerLineName extends AccountReportLineName {
-    static template = "account_reports_check_number.GeneralLedgerLineName";
-}
